f77784ad09e64ec47556207b84f3c6b1e04cda31
Emilio Gomez <emiliogmztj@gmail.com>
2024-03-29 15:48:03 -0700

    Fixed error.

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ def validate_folders():
         if not os.path.exists(folder):
             os.makedirs(folder)
             return
-        shutil.rmtree(folder)
-        os.makedirs(folder)
 
 def file_exists(file_path):
     if os.path.exists(file_path):
@@ -40,16 +38,17 @@ def main():
     validate_folders()
     if not file_exists(itemsFile):
         items = requests.get(itemsUrl)
-        with open(itemsFile, "w", encoding="utf-8") as itemPath:
+        with open(itemsFile, "w") as itemPath:
+            json.dump(items.json(), itemPath)
             itemPath.write(items.text)
     if not file_exists(monstersFile):
         pokemon = requests.get(monstersUrl)
-        with open(monstersFile, "w", encoding="utf-8") as pokemonPath:
-            pokemonPath.write(pokemon.text)
+        with open(monstersFile, "w") as pokemonPath:
+            json.dump(pokemon.json(), pokemonPath)
     if not file_exists(skillsFile):
         moves = requests.get(skillsUrl)
-        with open(skillsFile, "w", encoding="utf-8") as movePath:
-            movePath.write(moves.text)
+        with open(skillsFile, "w") as movePath:
+            json.dump(moves.json(), movePath)
     extract_abilities()
     extract_locations()
     populate()
@@ -63,17 +62,17 @@ redis_password = os.getenv("REDIS_PASSWORD")
 class DataTypes(Enum):
     ITEMS = 1
     POKEMON = 2
-    MOVES = 3,
-    ABILITIES = 4,
+    MOVES = 3
+    ABILITIES = 4
     LOCATIONS = 5
 
-redis_pokemon_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.POKEMON)
-redis_items_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.ITEMS)
-redis_moves_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.MOVES)
-redis_abilities_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.ABILITIES)
-redis_locations_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.LOCATIONS)
+redis_pokemon_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.POKEMON.value)
+redis_items_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.ITEMS.value)
+redis_moves_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.MOVES.value)
+redis_abilities_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.ABILITIES.value)
+redis_locations_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, db=DataTypes.LOCATIONS.value)
 
-def populate_edis(json_data, data_type):
+def populate_redis(json_data, data_type):
     for data in json_data:
         if(data_type == DataTypes.ITEMS):
             redis_items_client.set(data['id'], json.dumps(data))
@@ -88,31 +87,30 @@ def populate_edis(json_data, data_type):
         time.sleep(0.01)
 
 def populate():
-    with open(itemsFile,"r", encoding="utf-8") as itemData:
-        populate_edis(json.load(itemData), DataTypes.ITEMS)
-    with open(monstersFile,"r", encoding="utf-8") as monstersData:
-        populate_edis(json.load(monstersData), DataTypes.POKEMON)
-    with open(skillsFile,"r", encoding="utf-8") as skillsData:
-        populate_edis(json.load(skillsData), DataTypes.MOVES)
-    with open(abilitiesFile,"r", encoding="utf-8") as abilitiesData:
-        populate_edis(json.load(abilitiesData), DataTypes.ABILITIES)
-    with open(locationsFile,"r", encoding="utf-8") as locationsData:
-        populate_edis(json.load(locationsData), DataTypes.LOCATIONS)
+    with open(itemsFile) as itemData:
+        populate_redis(json.load(itemData), DataTypes.ITEMS)
+    with open(monstersFile) as monstersData:
+        populate_redis(json.load(monstersData), DataTypes.POKEMON)
+    with open(skillsFile) as skillsData:
+        populate_redis(json.load(skillsData), DataTypes.MOVES)
+    with open(abilitiesFile) as abilitiesData:
+        populate_redis(json.load(abilitiesData), DataTypes.ABILITIES)
+    with open(locationsFile) as locationsData:
+        populate_redis(json.load(locationsData), DataTypes.LOCATIONS)
     
 def extract_abilities():
-    with open(monstersFile, "r", encoding="utf-8") as pokemonData:
+    with open(monstersFile, "r") as pokemonData:
         pokemon = json.load(pokemonData)
         abilities = []
         for data in pokemon:
             for ability in data['abilities']:
                 if ability not in abilities:
                     abilities.append(ability)
-        with open(abilitiesFile, "w+", encoding="utf-8") as abilitiesData:
-            abilities.sort(key=lambda x: x['name'])
+        with open(abilitiesFile, "w") as abilitiesData:
             json.dump(abilities, abilitiesData)
 
 def extract_locations():
-    with open(monstersFile, "r", encoding="utf-8") as pokemonData:
+    with open(monstersFile) as pokemonData:
         pokemon = json.load(pokemonData)
         locations = []
         for data in pokemon:
@@ -124,8 +122,7 @@ def extract_locations():
                 }
                 if new_location not in locations:
                     locations.append(new_location)
-        with open(locationsFile, "w+", encoding="utf-8") as locationsData:
-            locations.sort(key=lambda x: x['region_id'])
+        with open(locationsFile, "w") as locationsData:
             json.dump(locations, locationsData)
 
 if __name__ == "__main__":
